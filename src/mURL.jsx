c07bf06badad75bcd26edc3451ab179345c272fa
farxod_0017 <farxodegamnazarov2@gmail.com>
2025-05-21 19:26:33 +0500

    mURL

--- src/mURL.jsx
+++ src/mURL.jsx
@@ -1,3 +1,3 @@
 // export const mURL = "https://apitarjimon.nurazizcom.uz/api"
 // export const mURL = "https://api.tourling.uz/api"
-export const mURL = "http://94.250.202.249:8000/api"
+export const mURL = "http://94.250.202.249:8000/api";
